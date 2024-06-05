2f6a90e737178c76412c9e821cae32d363d191d9
Иван Дюмин <dev@n1creator.com>
2024-06-05 18:22:11 +0300

    fx

--- src/Providers/SmsRu.php
+++ src/Providers/SmsRu.php
@@ -3,9 +3,6 @@
 namespace N1Creator\LaravelSmsNotifications\Providers;
 
 use N1Creator\LaravelSmsNotifications\Contracts\Provider;
-use N1Creator\LaravelSmsNotifications\Sms\Api;
-use N1Creator\LaravelSmsNotifications\Sms\ApiIdAuth;
-use N1Creator\LaravelSmsNotifications\Sms\Sms;
 use N1Creator\SmsRu as SmsRuApi;
 
 class SmsRu implements Provider
@@ -44,7 +41,7 @@ class SmsRu implements Provider
     public function send($phone, $text, array $options = []): bool
     {
         $response = $this->getClient()->smsSend(
-            $this->applyOptions(new Sms($phone, $text), $options)
+            $this->applyOptions(new SmsRuApi\Entity\Sms($phone, $text), $options)
         );
 
         return $this->checkResponse($response);
@@ -62,7 +59,7 @@ class SmsRu implements Provider
     public function sendBatch(array $phones, $message, array $options = []): bool
     {
         $smsList = array_map(function ($phone) use ($message, $options) {
-            return new $this->applyOptions(new Sms($phone, $message), $options);
+            return new $this->applyOptions(new SmsRuApi\Entity\Sms($phone, $message), $options);
         }, $phones);
         $response = $this->getClient()->smsSend(new SmsRuApi\Entity\SmsPool($smsList));
 
@@ -75,8 +72,8 @@ class SmsRu implements Provider
     private function getClient()
     {
         if (!$this->client) {
-            return $this->client = new Api(
-                new ApiIdAuth(
+            return $this->client = new SmsRuApi\Api(
+                new SmsRuApi\Auth\ApiIdAuth(
                     $this->options['api_id']
                 ),
                 new SmsRuApi\Client\Client()
